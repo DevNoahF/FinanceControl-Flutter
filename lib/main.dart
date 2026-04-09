20212e00516fdef8adeb4586cd9de365381e75d5
noah <noahvf16@outlook.com>
2026-04-09 20:23:15 -0300

    Refactor main.dart to remove unused imports and update app title and home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'pages/dashboard.dart';
-=======
-import 'pages/cadastro.dart'; // importa o arquivo da sua tela
->>>>>>> 62006ff9dfa2d8b904045f965ee444d09e4c2853
 
 void main() {
   runApp(const MyApp());
@@ -15,18 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       title: 'Finanças',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFE0E0E0)),
       home: const HomeScreen(),
-=======
-      title: 'Finance Control',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Cadastro(), 
->>>>>>> 62006ff9dfa2d8b904045f965ee444d09e4c2853
     );
   }
 }
