81836a79ec7d706ed291ea273b8631cb405bafd9
Leonardo Perin <155500125+Perinzinho@users.noreply.github.com>
2026-04-09 20:19:36 -0300

    Arrumei as páginas, agora rodam

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -8,8 +8,11 @@ class Cadastro extends StatelessWidget{
         children: [
           Expanded(child: Container(
             color: Colors.white,
-            child: Center(child: Image.asset("assets/download.jpg"),),
+            child: Center(child: Image.asset("assets/Fundo.jpeg"),),
           ))
+          BoxDecoration(
+            color: Color.fromRGBO(1, 46, 52, 59)
+          )
         ],
       ),
     );
