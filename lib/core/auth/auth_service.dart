d82bd4fab05475cd0f8318066699114829ee206a
Joaquim_Oliveira <yunohanayuunasan@gmail.com>
2026-05-05 20:54:36 -0300

    Cadastro funcionando como deveria

--- lib/core/auth/auth_service.dart
+++ lib/core/auth/auth_service.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:finance_control/models/usuario.dart';
 
 class AuthService extends ChangeNotifier {
   bool _isAuthenticated = false;
 
+  final List<Usuario> _usuarios = [
+    Usuario(
+      id: 1,
+      nome: 'Admin',
+      sobrenome: 'Teste',
+      email: 'admin@teste.com',
+      senha: '123',
+      idade: 20,
+      created_at: DateTime.now(),
+    ),
+  ];
+
   bool get isAuthenticated => _isAuthenticated;
 
+  List<Usuario> get usuarios => _usuarios;
+
+  void cadastrar(Usuario usuario) {
+    _usuarios.add(usuario);
+    notifyListeners();
+  }
+
   void login(BuildContext context, String email, String password) {
-    if (email == "admin@teste.com" && password == "123") {
+    final usuarioExiste = _usuarios.any(
+      (usuario) =>
+          usuario.email == email.trim() &&
+          usuario.senha == password.trim(),
+    );
+
+    if (usuarioExiste) {
       _updateAuth(true);
     } else {
-      // Alerta de erro simples
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Usuário ou senha incorretos")),
+        const SnackBar(
+          content: Text("Usuário ou senha incorretos"),
+        ),
       );
     }
   }
